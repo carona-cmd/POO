22439d2ca1ad1f3715afe841ea2c5124e72d395a
carona-cmd <alissars2001@yahoo.com>
2022-11-12 08:01:32 +0100

    adding method

--- introduction /notionpoo.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$product = json_decode(base64_decode('eyJuYW1lIjoiQ2FydGUgbVx1MDBlOHJlIiwic3RvY2siOiIxMiIsImJyYW5kIjoiQXN1cyJ9'), true);
-
-echo $product['name'].' ('.$product['brand'].') : '.$product['stock'];
